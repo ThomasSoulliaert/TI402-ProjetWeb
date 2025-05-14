f1c30ffe4e311017ba4887df01536af679c1bc4f
Thomas <tsoullia@gmail.com>
2025-05-15 01:05:51 +0200

    final

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,29 @@ window.addEventListener("scroll", function () {
     header.classList.remove("shrink");
   }
 });
+
+
+/* Espèces */
+
+const filtres = document.querySelectorAll('.filtre');
+const resetBtn = document.getElementById('reset-filtres');
+const animaux = document.querySelectorAll('.animal');
+
+function filtrerAnimaux() {
+    const filtresActifs = Array.from(filtres)
+      .filter(f => f.checked)
+      .map(f => f.value);
+
+    animaux.forEach(animal => {
+      const tags = animal.dataset.tags.split(' ');
+      const visible = filtresActifs.every(f => tags.includes(f));
+      animal.style.display = visible || filtresActifs.length === 0 ? 'block' : 'none';
+    });
+  }
+
+  filtres.forEach(f => f.addEventListener('change', filtrerAnimaux));
+
+  resetBtn.addEventListener('click', () => {
+    filtres.forEach(f => f.checked = false);
+    filtrerAnimaux();
+});
